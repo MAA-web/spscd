fdf893d41435932ac9bf438f1372bddd53ff3434
kilja <zigzag42471129@gmail.com>
2021-01-05 11:57:15 -0500

    3rd commit i am so bored also child labour

--- Main.js
+++
@@ -1,7 +0,0 @@
-// hihihihihi
-//document.getElementsByClassName("card text-center").addhtml("<img src="https://www.google.com/search?q=poor+children&tbm=isch&ved=2ahUKEwiKn5vd6YLuAhUEcxoKHXaFAHYQ2-cCegQIABAA&oq=poor+children&gs_lcp=CgNpbWcQDFAAWABguzJoAHAAeACAAQCIAQCSAQCYAQCqAQtnd3Mtd2l6LWltZw&sclient=img&ei=a1PzX8q9LYTmafaKgrAH&bih=776&biw=1406&hl=en-US#imgrc=GsVQ7dt1TKrc9M"></img>")
-//https://www.google.com/search?q=poor+children&tbm=isch&ved=2ahUKEwiKn5vd6YLuAhUEcxoKHXaFAHYQ2-cCegQIABAA&oq=poor+children&gs_lcp=CgNpbWcQDFAAWABguzJoAHAAeACAAQCIAQCSAQCYAQCqAQtnd3Mtd2l6LWltZw&sclient=img&ei=a1PzX8q9LYTmafaKgrAH&bih=776&biw=1406&hl=en-US#imgrc=GsVQ7dt1TKrc9M
-document.getElementById("btn btn-primary").onclick = function() {
-    window.location.href = "Aboutus.html";
-    //window.location.replace("/home/mockingjay/Documents/work/sadiqpublicschoolclotingdrive.42web.io/spscd/Aboutus.html")
-}
---
+++ Petya.js
@@ -0,0 +1,4 @@
+//https://www.google.com/search?q=poor+children&tbm=isch&ved=2ahUKEwiKn5vd6YLuAhUEcxoKHXaFAHYQ2-cCegQIABAA&oq=poor+children&gs_lcp=CgNpbWcQDFAAWABguzJoAHAAeACAAQCIAQCSAQCYAQCqAQtnd3Mtd2l6LWltZw&sclient=img&ei=a1PzX8q9LYTmafaKgrAH&bih=776&biw=1406&hl=en-US#imgrc=GsVQ7dt1TKrc9M
+document.getElementById("btn btn-primary").onclick = function() {
+    location.href = "Aboutus.html";
+}
