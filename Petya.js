9bf36976dffbc6788c35a586314508e22e8b55c7
kilja <zigzag42471129@gmail.com>
2021-01-06 16:43:05 -0500

    i am brain dead

--- Petya.js
+++ Petya.js
@@ -1,4 +1,10 @@
 //https://www.google.com/search?q=poor+children&tbm=isch&ved=2ahUKEwiKn5vd6YLuAhUEcxoKHXaFAHYQ2-cCegQIABAA&oq=poor+children&gs_lcp=CgNpbWcQDFAAWABguzJoAHAAeACAAQCIAQCSAQCYAQCqAQtnd3Mtd2l6LWltZw&sclient=img&ei=a1PzX8q9LYTmafaKgrAH&bih=776&biw=1406&hl=en-US#imgrc=GsVQ7dt1TKrc9M
-document.getElementById("btn btn-primary").onclick = function() {
+
+console.log(screen.width)
+var width = screen.width
+var halfwidth = width/2
+
+document.getElementById("donate").onclick = function() {
     location.href = "Aboutus.html";
 }
+
---
+++ Style.js
@@ -0,0 +1,11 @@
+var width = screen.width
+var height = screen.height
+var ws = width/1.8 - 200
+var halfwidth = ws + "px"
+var hs = height/4
+var quarterheight = hs + "px"
+
+document.getElementById("school-image").style.marginLeft = halfwidth
+
+document.getElementById("school-image").style.marginTop = quarterheight
+document.getElementById("main-notes").style.marginTop = quarterheight
